da0172f47480564557b9344dde8b6c7aa3e3138e
abuaesh <noha.abuaesh@gmail.com>
2020-08-25 05:09:44 +0300

    Edited deployer to include verifier address

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,24 @@
 // migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+var SquareVerifier = artifacts.require("../../zokrates/code/square/verifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+  //deployer.deploy(SquareVerifier);
+  //deployer.deploy(SolnSquareVerifier);
+
+  deployer.deploy(SquareVerifier)
+    .then(() => {
+        return deployer.deploy(SolnSquareVerifier, SquareVerifier.address)
+                .then(() => {
+                    let config = {
+                        localhost: {
+                            url: 'http://localhost:7545',
+                            verifierAddress: SquareVerifier.address,
+                            solnAddress: SolnSquareVerifier.address
+                        }
+                    }
+                    //fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                    //fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                });
+    });
 };
